0cac6e170e7d8e32bbe5cbad571b58a8ace6bf1c
Axel Leguía <axeleguia.chero@outlook.com>
2023-03-09 00:57:46 -0500

    First upload

---
+++ README.md
@@ -0,0 +1,33 @@
+<p align="center">
+  <a href="https://www.java.com" target="blank"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/300px-Java_programming_language_logo.svg.png" width="200" alt="Nest Logo" /></a>
+</p>
+
+<p align="center">
+    A simple <a href="https://www.java.com" target="_blank">Java</a> app for practical purposes using JUnit
+</p>
+
+## Description
+
+Java and JUnit 5 Practical examples.
+
+Below are the topics implemented:
+
+### Basics:
+1. Test exceptions
+2. Test with multiple assertions
+3. Test null values
+4. Test array equality
+5. @BeforeEach and @AfterEach
+6. @BeforeAll and @AfterAll
+
+### More Advanced Concepts:
+1. Parameterized Tests - ValueSource
+2. Parameterized Tests - CsvSource
+3. Parameterized Tests - CsvFileSource
+4. Repeated Test
+5. Test performance
+6. Assumptions
+7. Organize your code with @Nested
+8. Other annotations
+
+#### Credits to [Adrian Wiech](https://www.udemy.com/user/adrian-wiech/) and their course [Practical Java Unit Testing with JUnit 5](https://www.udemy.com/course/junit5-for-beginners/)
---
+++ test/main/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,85 @@
+package main.java.com.healthycoderapp;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all unit test.");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all unit test.");
+    }
+
+    @Test
+    void should_ReturnTrue_When_DietRecommended() {
+        //arrange
+        double weight = 89.0;
+        double height = 1.72;
+        //act
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+        //assert
+        assertTrue(recommended);
+    }
+
+    @Test
+    void should_ReturnFalse_When_DietRecommended() {
+        //given
+        double weight = 50.0;
+        double height = 1.92;
+        //when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+        //then
+        assertFalse(recommended);
+    }
+
+    @Test
+    void should_ThrowArithmeticException_When_HeightZero() {
+        double weight = 50.0;
+        double height = 0;
+        Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
+        assertThrows(ArithmeticException.class, executable);
+    }
+
+    @Test
+    void should_ReturnCoderWithWorstBMI_When_CoderListNotEmpty() {
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80, 60.0));
+        coders.add(new Coder(1.82, 98.0));
+        coders.add(new Coder(1.82, 64.7));
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+        assertAll(
+                () -> assertEquals(1.82, coderWorstBMI.getHeight()),
+                () -> assertEquals(98, coderWorstBMI.getWeight())
+        );
+    }
+
+    @Test
+    void should_ReturnNullWorstBMI_When_CoderListEmpty() {
+        List<Coder> coders = new ArrayList<>();
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+        assertNull(coderWorstBMI);
+    }
+
+    @Test
+    void should_ReturnCorrectBMIScoreArray_When_CoderListEmpty() {
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80, 60.0));
+        coders.add(new Coder(1.82, 98.0));
+        coders.add(new Coder(1.82, 64.7));
+        double[] expected = {18.52, 29.59, 19.53};
+        double[] bmiScores = BMICalculator.getBMIScores(coders);
+        assertArrayEquals(expected, bmiScores);
+    }
+}
